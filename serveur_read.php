b46fe63b48c459099aedc59da0b483e7ffd754d9
brian-bourdon <brian78.b@gmail.com>
2019-04-11 12:25:19 +0200

    Add files via upload

--- serveur_read.php
+++ serveur_read.php
@@ -2,11 +2,11 @@
 	if($file = fopen('message', 'a+'))
 	{
 		if($line = fgets($file)) echo "Message: ".$line;
+		fclose($file);
 		
 	}
 	else echo "Error when reading the file";
-	
-	fclose($file);
+
 	
 	if($line == "hello") echo "</br></br>Succeed";
 	else echo "</br></br>Failed";
